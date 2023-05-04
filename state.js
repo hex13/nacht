62269f17ddde9717582821d87253a0df03500905
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-05-04 15:26:33 +0200

    Emitter

--- state.js
+++ state.js
@@ -17,8 +17,42 @@ export function State(value) {
             return () => {
                 listeners = listeners.filter(x => x !== listener);
             };
+        },
+    }
+}
+
+export function Emitter(eventTarget) {
+    const observables = Object.create(null);
+    return {
+        on(type) {
+            let subject = observables[type];
+            if (!subject) {
+                subject = State();
+                eventTarget && eventTarget.addEventListener(type, (e) => {
+                    subject.set(e);
+                });
+            }
+            observables[type] = subject;
+            return subject;
         }
     }
 }
 
 export const IS_STATE = Symbol('State');
+
+// testing
+const em = new Emitter();
+const click = em.on('click');
+click.subscribe(e => {
+    console.log("emitter, value", e);
+})
+const click1 = em.on('click');
+click.set(120);
+click1.set(460);
+
+
+const doc = new Emitter(document);
+
+doc.on('click').subscribe(e => {
+    console.log("document clicked:", e);
+})
