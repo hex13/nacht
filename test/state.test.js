9fae0c97cff73d408f14f6606d5718d71477db7a
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-05-05 15:52:28 +0200

    tests for state.js

---
+++ test/state.test.js
@@ -0,0 +1,64 @@
+import * as assert from 'node:assert';
+import { State, IS_STATE } from '../state.js';
+
+describe('State', () => {
+    it('should have non-falsy [IS_STATE] property', () => {
+        assert.ok(State()[IS_STATE]);
+    });
+    it('.get() should return initial value after creating and it should be possible to .set() and .get() values', () => {
+        const state = State(9);
+        assert.strictEqual(state.get(), 9);
+        state.set(19);
+        assert.strictEqual(state.get(), 19);
+        state.set(2);
+        assert.strictEqual(state.get(), 2);
+    });
+    describe('.subscribe() should allow for subscribing and', () => {
+        it('setting new values via .set() should get listeners called', (done) => {
+            const state = State(1);
+            const events = [];
+            state.subscribe(action => {
+                events.push(['first', action]);
+            });
+            state.subscribe(action => {
+                events.push(['second', action]);
+            });
+            state.set('one beer');
+            state.set('zero beers');
+            state.set('999 beers');
+            setTimeout(() => {
+                assert.deepStrictEqual(events, [
+                    ['first', {type: 'set', oldValue: 1, newValue: 'one beer'}],
+                    ['second', {type: 'set', oldValue: 1, newValue: 'one beer'}],
+                    ['first', {type: 'set', oldValue: 'one beer', newValue: 'zero beers'}],
+                    ['second', {type: 'set', oldValue: 'one beer', newValue: 'zero beers'}],
+                    ['first', {type: 'set', oldValue: 'zero beers', newValue: '999 beers'}],
+                    ['second', {type: 'set', oldValue: 'zero beers', newValue: '999 beers'}],
+                ]);
+                done();
+            }, 0);
+        });
+        it('.trigger() should get listeners called with passed action', (done) => {
+            const state = State();
+            const events = [];
+            state.subscribe(action => {
+                events.push(['first', action]);
+            });
+            state.subscribe(action => {
+                events.push(['second', action]);
+            });
+            state.trigger({type: 'foo', a: 1});
+            state.trigger({type: 'bar', a: 2});
+
+            setTimeout(() => {
+                assert.deepStrictEqual(events, [
+                    ['first', {type: 'foo', a: 1}],
+                    ['second', {type: 'foo', a: 1}],
+                    ['first', {type: 'bar', a: 2}],
+                    ['second', {type: 'bar', a: 2}],
+                ]);
+                done();
+            }, 0);
+        });
+    })
+});
